1de5643780aa8ccc94c47c3fbd12a3018f94bceb
Eimert Vink <eimertvink@gmail.com>
2018-08-13 15:14:56 +0200

    initial Mockito unit test, testing a few db operations

--- src/test/java/nl/eimertvink/salaryapi/EmployeeRepositoryTest.java
+++ src/test/java/nl/eimertvink/salaryapi/EmployeeRepositoryTest.java
@@ -3,30 +3,27 @@ package nl.eimertvink.salaryapi;
 import nl.eimertvink.configuration.PersistenceJPAConfig;
 import nl.eimertvink.model.Employee;
 import nl.eimertvink.repository.EmployeeRepository;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmployeeRepositoryTest {
 
-    @Autowired
+    @Mock
     EmployeeRepository employeeRepository;
 
     @Mock
     PersistenceJPAConfig persistenceJPAConfig;
 
     @Test(timeout = 200)
-    @Ignore
+//    @Ignore
     public void queryByMaxInternalSalaryTest() {
         employeeRepository.queryByMaxInternalSalary();
     }
 
     @Test
-    @Ignore
     public void saveTest() {
         Employee e = new Employee();
         e.setName("Carl Black");
