1ac9815601c47cb8422f59dbbd1ea7c51090780b
Eimert Vink <eimertvink@gmail.com>
2020-07-12 16:53:15 +0200

    Fix slow insert issue, now using csv. Using latest dataset from February 1, 2020.

--- src/main/java/nl/eimertvink/entity/Salaries.java
+++ src/main/java/nl/eimertvink/entity/Salaries.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -18,10 +18,24 @@ public class Salaries extends AbstractEntity {
     @Size(min = 3) @Getter @Setter
     private String name;
     @NotNull @Getter @Setter
-    private String position;
+    @Column(name = "jobtitles")
+    private String jobTitles;
     @NotNull @Getter @Setter
     private String department;
-    @Min(0) @Getter @Setter
-    private Double salary;
+    @Getter @Setter
+    @Column(name = "fullorparttime")
+    private String fullOrPartTime;
+    @Getter @Setter
+    @Column(name = "salaryorhourly")
+    private String salaryOrHourly;
+    @Getter @Setter
+    @Column(name = "typicalhours")
+    private String typicalHours;
+    @Getter @Setter
+    @Column(name = "annualsalary")
+    private Double annualSalary;
+    @Getter @Setter
+    @Column(name = "hourlyrate")
+    private Double hourlyRate;
 
 }
